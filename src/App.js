b19230c5163bb4a24237d691e4ea50a8f4215890
Bakhyt Sembayev <bahytsembaev@gmail.com>
2024-02-28 21:30:49 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import logo from './logo.svg';//декларативный стиль весь код
-import {MyComponent} from './MyComponent';
+import React from 'react';
+import logo from './logo.svg';
+import { MyComponent } from './MyComponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload 3.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MyComponent />
-        
-      </header>
-    </div>
+    React.createElement('div', { className: 'App' },
+      React.createElement('header', { className: 'App-header' },
+        React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+        React.createElement('p', null, 
+          'Edit ',
+          React.createElement('code', null, 'src/App.js'),
+          ' and save to reload 3.'
+        ),
+        React.createElement('a', { 
+          className: 'App-link', 
+          href: 'https://reactjs.org', 
+          target: '_blank', 
+          rel: 'noopener noreferrer' 
+        }, 'Learn React'),
+        React.createElement(MyComponent, null)
+      )
+    )
   );
-};
-
+}
 
 export default App;
+
